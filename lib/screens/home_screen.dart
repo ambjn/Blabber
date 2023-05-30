73b1d5646b4cefba9406691d2b93b4d1a4d2c65c
Amber Jain <jainanuamber@gmail.com>
2023-05-30 10:34:16 +0530

    updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:blabber/theme/pallete.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +11,40 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'Blabber💬',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        leading: const Icon(Icons.menu),
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 5),
+                  decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle),
+                ),
+              ),
+              Container(
+                height: 118,
+                decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/blabber_logo.jpeg'))),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
